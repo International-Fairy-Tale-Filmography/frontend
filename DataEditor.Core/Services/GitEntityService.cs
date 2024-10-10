dbdc08157e7850b9c03be612786c37cff3830b58
Jeff Lefebvre <thestamp@gmail.com>
2024-10-10 00:09:02 -0500

    WIP saving to git

--- DataEditor.Core/Services/GitEntityService.cs
+++ DataEditor.Core/Services/GitEntityService.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using CsvHelper;
 using CsvHelper.Configuration;
 using Microsoft.EntityFrameworkCore;
@@ -15,24 +18,45 @@ namespace DataEditor.Core.Services
     public class GitEntityService(DataEditorDataContext context, CoreSettingsModel settings, GitService gitService)
     {
 
-        public async Task SaveChangesToGit()
+        public async Task<string> CommitChangesToGit()
         {
+            var filename = "Film.csv";
+            var filePath = Path.Combine(settings.Folder, filename);
+            var file = await GetFileByName(filename);
+
             var films = await context.Films.ToListAsync();
 
             await using var writer = new StringWriter();
             await using var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture));
+            csv.WriteHeader<Film>();
+            await csv.NextRecordAsync();
             await csv.WriteRecordsAsync(films);
+
+            var content = writer.ToString();
+
+            var result = await gitService.UpdateFile("Film.csv", file, content, $"test update at {DateTime.Now}");
+
+             return result.Commit.Sha;
+             // Snackbar.Add("Saved commit " + result.Commit.Sha, Severity.Success);
+        }
+
+
+        public async Task<RepositoryContent> GetFileByName(string name)
+        {
+            var filePath = Path.Combine(settings.Folder, name);
+            var file = await gitService.GetFile(filePath);
+            return file;
         }
 
 
         public async Task SeedDataFromGit()
         {
-            //context.Companies.AddRange(await FetchCsv<Company>("Company"));
-            //context.Countries.AddRange(await FetchCsv<Country>("Country"));
-            context.Films.AddRange(await FetchCsv<Film>("Film"));
-            //context.Languages.AddRange(await FetchCsv<Language>("Language"));
-            //context.Origins.AddRange(await FetchCsv<Origin>("Origin"));
-            //context.People.AddRange(await FetchCsv<Person>("Person"));
+            //context.Companies.AddRange(await FetchCsv<Company>("Company.csv"));
+            //context.Countries.AddRange(await FetchCsv<Country>("Country.csv"));
+            context.Films.AddRange(await FetchCsv<Film>("Film.csv"));
+            //context.Languages.AddRange(await FetchCsv<Language>("Language.csv"));
+            //context.Origins.AddRange(await FetchCsv<Origin>("Origin.csv"));
+            //context.People.AddRange(await FetchCsv<Person>("Person.csv"));
             //await context.SaveChangesAsync();
 
             //await MapCompanyFilms();
@@ -44,8 +68,7 @@ namespace DataEditor.Core.Services
 
         private async Task<List<T>> FetchCsv<T>(string name)
         {
-            var filePath = Path.Combine(settings.Folder, name) + ".csv";
-            var file = await gitService.GetFile(filePath);
+            var file = await GetFileByName(name);
 
             using var reader = new StringReader(file.Content);
 
@@ -58,7 +81,7 @@ namespace DataEditor.Core.Services
 
         private async Task MapCompanyFilms()
         {
-            var companyFilms = await FetchCsv<CompanyFilm>("CompanyFilm");
+            var companyFilms = await FetchCsv<CompanyFilm>("CompanyFilm.csv");
 
             var filmDict = context.Films.ToDictionary(i => i.FilmId, i => i);
             var companyDict = context.Companies.ToDictionary(i => i.CompanyId, i => i);
@@ -78,7 +101,7 @@ namespace DataEditor.Core.Services
 
         private async Task MapCountryFilms()
         {
-            var companyFilms = await FetchCsv<CountryFilm>("CountryFilm");
+            var companyFilms = await FetchCsv<CountryFilm>("CountryFilm.csv");
 
             var filmDict = context.Films.ToDictionary(i => i.FilmId, i => i);
             var countryDict = context.Countries.ToDictionary(i => i.CountryId, i => i);
