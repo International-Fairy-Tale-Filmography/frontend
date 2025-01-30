2627e448e9c79409a9aa0c57281290e4eac0d08c
Jeff Lefebvre <thestamp@gmail.com>
2025-01-30 00:34:20 -0600

    fixed csv url paths, temporarily removed 1-m relationships due to editor refactor, added initial film page and added valid film links

--- FrontEnd.Web/Program.cs
+++ FrontEnd.Web/Program.cs
@@ -1,8 +1,11 @@
+using FrontEnd.Core.Configuration;
 using FrontEnd.Core.Data;
+using FrontEnd.Core.Services;
 using FrontEnd.Web;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.EntityFrameworkCore;
+using static System.Net.WebRequestMethods;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,7 +13,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<FrontEndDataContext>();
+builder.Services.AddScoped<CoreSettingsModel>(i => new CoreSettingsModel()
+{
+    CsvBaseUrl = "https://international-fairy-tale-filmography.github.io/data/csv/"
+});
+builder.Services.AddScoped<FrontEndDataInitializerService>();
 
-builder.Services.AddSingleton<FrontEndDataContext>();
+
 
 await builder.Build().RunAsync();
